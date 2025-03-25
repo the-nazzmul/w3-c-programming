0931f8e8209fab6f70e24b382f2f849d979b485a
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-03-25 17:36:34 +0600

    learning output

---
+++ many_printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     printf("Hello World!");
+//     printf("I am learning C");
+//     return 0;
+// }
+
+// C basically doesn't add new line by default. The code above will not add new line after "Hello World!", rather it would kind of print everything on the same line
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("I am learning C.\n");
+    return 0;
+}
+
+// "\n" has to be added at the end of each line to instruct C that there will be a new line starting from here
