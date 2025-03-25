97ea3c1b06a7dd4f6d1b7102f5a16b87f23a1842
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-03-25 17:51:28 +0600

    some more valid escape sequence

--- many_printf.c
+++ many_printf.c
@@ -9,11 +9,44 @@
 
 // C basically doesn't add new line by default. The code above will not add new line after "Hello World!", rather it would kind of print everything on the same line
 
-int main()
-{
-    printf("Hello World!\n");
-    printf("I am learning C.\n");
-    return 0;
-}
+// int main()
+// {
+//     printf("Hello World!\n");
+//     printf("I am learning C.\n");
+//     return 0;
+// }
 
 // "\n" has to be added at the end of each line to instruct C that there will be a new line starting from here
+
+// Some other iterations
+
+// int main()
+// {
+//     printf("Hello World!\nI am learning C.\n\nAnd it is awesome!");
+//     return 0;
+// }
+
+// Two \n creates a blank line
+
+//  "\t" creates a horizontal tab
+// int main()
+// {
+//     printf("Hello World!\t");
+//     printf("I am learning C.");
+//     return 0;
+// }
+
+//  "\\" inserts a backslash character(\)
+// int main()
+// {
+//     printf("Hello World!\\");
+//     printf("I am learning C.");
+//     return 0;
+// }
+
+//  '\"' inserts a double quote character
+int main()
+{
+    printf("People close to me call me \"Utchchash\".");
+    return 0;
+}
