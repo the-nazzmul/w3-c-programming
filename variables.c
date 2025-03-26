0f04a6515e9877f542e9537b1321588685f5d1d7
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2025-03-26 15:43:33 +0600

    explored different format specifiers

--- variables.c
+++ variables.c
@@ -13,13 +13,36 @@ char - stores single characters, such as 'a' or 'B'. Char values are surrounded 
 //     return 0;
 // }
 
+// int main()
+// {
+//     int myNum = 15;
+//     printf("%d", myNum);
+//     return 0;
+// }
+
+/*Format specifiers are used together with the printf() function to tell the compiler what type of data the variable is storing. It is basically a placeholder for the variable value.
+
+A format specifier starts with a percentage sign %, followed by a character. To output the value of an int variable, we need to use the format specifier %d surrounded by double quotes(""), inside the printf() function.
+
+To print other types, we need to use %c for char and %f for float
+*/
+
 int main()
 {
-    int myNum = 15;
-    printf("%d", myNum);
-    /*Format specifiers are used together with the printf() function to tell the compiler what type of data the variable is storing. It is basically a placeholder for the variable value.
+    int myNum = 1;
+    float myFloatNum = 5.66;
+    char myLetter = 'N';
 
-    A format specifier starts with a percentage sign %, followed by a character. To output the value of an int variable, we need to use the format specifier %d surrounded by double quotes(""), inside the printf() function
-    */
+    // print variables
+    // printf("%d\n", myNum);
+    // printf("%f\n", myFloatNum);
+    // printf("%c\n", myLetter);
+
+    // combine both text and a variable
+    // printf("My favorite number is:%d\n", myNum);
+
+    // print different types in a single printf() function
+    printf("My favorite number is %d and my letter is %c\n", myNum, myLetter); // It is possible to print values without storing them in a variable as well
+
     return 0;
 }
